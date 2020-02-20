7a009b1dd9f514958df7b13e97e8eaa53782960e
ZhiYuan7 <1582306402@qq.com>
2020-02-20 11:42:09 +0800

    1

---
+++ Webpack/src/index.js
@@ -0,0 +1,6 @@
+import img from './img/24.jpg';
+import style from './css/test.scss';
+console.log('1111');
+var oImg = new Image();
+oImg.src = img;
+document.body.append(oImg);
---
+++ Webpack/webpack.config.js
@@ -0,0 +1,51 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const{ CleanWebpackPlugin} = require("clean-webpack-plugin");
+module.exports = {
+    mode:"development",
+    entry:"./src/index.js",
+    output:{
+        filename:"xxxxxxxxx.js",
+        path:path.resolve(__dirname,"dist")
+    },
+    module:{
+        rules:[
+            {
+                test:/\.(jpg|png|gif)$/,
+                use:{
+                    loader:'url-loader',
+                    options:{
+                        outputPath:'images/',
+                        limit:100,
+                        name:'[name].[ext]'
+                    }
+                   
+                }
+            },
+            {
+                
+                test:/\.css$/,
+                use:[ 
+                    "style-loader",
+                    "css-loader"
+                ]
+            },
+            {
+                
+                test:/\.scss$/,
+                use:[ 
+                    "style-loader",
+                    "css-loader",
+                    "postcss-loader",
+                    "sass-loader"
+                ]
+            }
+        ]
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            template:"./src/index.html"
+        }),
+        new CleanWebpackPlugin()
+    ]
+}
